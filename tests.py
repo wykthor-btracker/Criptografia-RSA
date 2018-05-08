31dde088c7ead193d803838177fcb5406b3a9896
Wykthor <wykthor.g@wgvale.com>
2018-05-08 04:30:35 -0300

    Added test to extended Gcd

--- tests.py
+++ tests.py
@@ -2,16 +2,27 @@
 import rsa
 import prime_gen as pg
 from primes_list import less_than_hundred_thousand
-
+from random import randint
 #imports#
 
 #classes
 #classes#
 
 #functions
+def gcd(a,b):
+    if(b == 0):
+        return(a)
+    else:
+        return gcd(b,a%b)
+
 def testPrimesGenerator():
     assert(pg.rabin_miller_is_prime(pg.generate_random_prime(1024)))
-
+def testExtendedGcd():
+    a,b = randint(0,1024),randint(0,1024)
+    x,y = rsa.extended_gcd(a,b)
+    greaterCommonDivisor = gcd(a,b)
+    result = x*a+y*b
+    assert(result==greaterCommonDivisor)
 #functions#
 
 #variables
