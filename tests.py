61ea570a7f20a12f8c0bdbf6013ba7e1735edc96
Wykthor <wykthor.g@wgvale.com>
2018-05-10 09:37:14 -0300

    Revert "Removing last test to check if it's hanging CI"
    This reverts commit 2940a64241b0752bbf8516e83556115170a37264.

--- tests.py
+++ tests.py
@@ -30,7 +30,7 @@ def testExtendedGcd():
     result = x*a+y*b
     assert(result==greaterCommonDivisor)
 
-def AtestPrimeChecker():
+def testPrimeChecker():
     for i in range(500):
         val = randint(2,2048)
         a = pg.primeChecker(val)
